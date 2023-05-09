bf13cd0bbb7dd00b604e92b8e27e3567a39b0df5
Ashley Johnson <ajohnson100920@gmail.com>
2023-05-09 15:15:51 -0400

    db

---
+++ db/db_connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const dbConfig = {
+    host: "sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com",
+    port: 3306,
+    user: "ashjoh25",
+    password: "5dU39qAuNgtK",
+    database: "New SQL Class User",
+    connectTimeout: 10000
+}
+
+const connection = mysql.createConnection(dbConfig);
+
+module.exports = connection;
