c8b82e659f32a920b8142e4a5a5720597867bd55
J-Tell <fairyq007@hotmail.com>
2023-05-26 14:47:11 -0400

    shrug idk

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,11 +1,26 @@
+// const mysql = require('mysql2');
+
+// const dbConfig = {
+//     host: "sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com",
+//     port: 3306,
+//     user: "ashjoh25",
+//     password: "5dU39qAuNgtK",
+//     database: "New SQL Class User",
+//     connectTimeout: 10000
+// }
+
+// const connection = mysql.createConnection(dbConfig);
+
+// module.exports = connection;
+
 const mysql = require('mysql2');
 
 const dbConfig = {
-    host: "sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com",
+    host: "<hostname>",
     port: 3306,
-    user: "ashjoh25",
-    password: "5dU39qAuNgtK",
-    database: "New SQL Class User",
+    user: "<username>",
+    password: "<password>",
+    database: "<schema>",
     connectTimeout: 10000
 }
 
--- db/db_test.js
+++ db/db_test.js
@@ -1,13 +1,13 @@
 const db = require("./db_connection");
 
-// Add query request 
+// Execute query, print results or error 
 db.execute('SELECT 1 + 1 AS solution', 
     (error, results) => {
         if (error)
             throw error;
         console.log(results);
-        console.log(`Solution: ${results[0].solution}`);
     }
 );
-//Optional: close the connection after queue is emptying.
+
+//Optional: close the connection after query queue is empty.
 db.end();
