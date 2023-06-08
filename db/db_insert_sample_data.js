f24323f6107f4fc39eda087cdbeeb93f33b2962a
J-Tell <fairyq007@hotmail.com>
2023-06-08 13:23:14 -0400

    I GOT THE TABLES WORKING!!!!!

--- db/db_insert_sample_data.js
+++ db/db_insert_sample_data.js
@@ -15,35 +15,31 @@ db.execute(delete_genres_table_sql);
 
 const insert_genre_sql = `
     INSERT INTO genres 
-        (genreId, genreName) 
+        (genreName) 
     VALUES 
-        (?, ?);
+        (?);
 `
 
-db.execute(insert_genre_sql, [1, 'Fantasy']);
+db.execute(insert_genre_sql, ['Fantasy']);
 
-db.execute(insert_genre_sql, [2, 'Romance']);
+db.execute(insert_genre_sql, ['Romance']);
 
-db.execute(insert_genre_sql, [3, 'Drama']);
+db.execute(insert_genre_sql, ['Drama']);
 
-db.execute(insert_genre_sql, [4, 'Comedy']);
+db.execute(insert_genre_sql, ['Comedy']);
 
 
 const insert_shows_sql = `
     INSERT INTO shows
-        (showTitle, genreId, dayFinished, description) 
+        (name, genre_id, description) 
     VALUES 
-        (?, ?, ?, ?);
+        (?, ?, ?);
 `
 
-//subjectId: 2 => 'Math'
-db.execute(insert_shows_sql, ['help', 1, '2023-05-26', 
-        'Do odd questions in the range #155 - #207 (chapter 11). Remember to show your work!']);
+db.execute(insert_shows_sql, ['help', 1, 'help!!!!!!']);
 
-//subjectId: 3 => 'Language'
-db.execute(insert_shows_sql, ['pls send help', 3, '2023-06-01', 'asdfgh']);
+db.execute(insert_shows_sql, ['pls send help', 3, 'asdfgh']);
 
-//subjectId: 1 => 'Comp Sci'
-db.execute(insert_shows_sql, ['ahhh', 4, '2023-06-07', 'poiuytree']);
+db.execute(insert_shows_sql, ['ahhh', 4, 'poiuytree']);
 
 db.end();
