145d3535cc68ee4f685029709899fe996ccb0521
J-Tell <fairyq007@hotmail.com>
2023-06-13 15:14:26 -0400

    um a lot of stuff but you gotta install some stuff

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ const app = express();
 const port = 3600;
 const logger = require("morgan");
 const DEBUG = true;
+const db = require('./db/db_connection');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+// // Load Node modules
+// // Render static files
+// app.use(express.static('public'));
+
+// *** GET Routes - display pages ***
+// Root Route
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
 // // define middleware that logs all incoming requests
 // app.use((req, res, next) => {
 //     console.log(`${req.method} ${req.url}`);
 //     next();
 // } );
-const db = require('./db/db_connection');
 
 app.use(logger("dev"));
 app.use(express.static(__dirname + '/public'));
@@ -18,111 +30,43 @@ app.use(express.static(__dirname + '/public'));
 
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/views/index.html" );
+    res.sendFile( __dirname + "/views/index" );
 } );
 
 // define a route for the assignment list page
 app.get( "/user_profile", ( req, res ) => {
-    res.sendFile( __dirname + "/views/user_profile.html" );
+    res.sendFile( __dirname + "/views/user_profile" );
 } );
 
+
 // define a route for the assignment list page
+const read_newrank_all_sql = `
+    SELECT 
+        show_id, name, description, shows.genre_id as genre_id
+    FROM shows
+    JOIN genres
+        ON shows.genre_id = genres.genre_id
+`
 app.get( "/add_ranking", ( req, res ) => {
-    res.sendFile( __dirname + "/views/add_ranking.html" );
-} );
+    db.execute(read_newrank_all_sql, (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else
+            res.send(results);
+    });
+});
 
 // define a route for the assignment detail page
 app.get( "/add_ranking/all_rankings", ( req, res ) => {
-    res.sendFile( __dirname + "/views/all_rankings.html" );
+    res.sendFile( __dirname + "/views/all_rankings" );
 } );
 
 // start the server
 app.listen( port, () => {
     console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
 } );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
