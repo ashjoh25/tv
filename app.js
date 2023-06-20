f044806ed331e9663f3cc55dd05b8a0b5c1e86a2
J-Tell <fairyq007@hotmail.com>
2023-06-19 23:18:44 -0400

    some cleanup changes

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
+const DEBUG = true;
+
 //set up the server
 const express = require( "express" );
-const app = express();
-const port = 3600;
 const logger = require("morgan");
-const path = require("path");
-const fs = require("fs");
-const DEBUG = true;
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 const dotenv = require('dotenv');
 dotenv.config();
 const db = require('./db/db_connection');
+const app = express();
+const port = 3000;
+
 // Configure Express to use EJS
-app.set( "views",  path.join(__dirname , "views"));
+app.set( "views",  __dirname + "/views");
 app.set( "view engine", "ejs" );
 
-const { auth } = require('express-openid-connect');
-
-// app.use(helmet({...
-// });
-
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: 'a long, randomly-generated string stored in env',
-    baseURL: 'http://localhost:<PORT>',
-    clientID: '<YOUR CLIENT ID>',
-    issuerBaseURL: 'https://<YOUR DOMAIN>.us.auth0.com'
-  };
-  app.use(auth(config));
-
-// Configure Express to parse URL-encoded POST request bodies (forms)
+// Configure Express to parse URL-encoded POST request bodies (traditional forms)
 app.use( express.urlencoded({ extended: false }) );
-// *** GET Routes - display pages ***
 
-
-app.get('/authtest', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
-
-// // define middleware that logs all incoming requests
-// app.use((req, res, next) => {
-//     console.log(`${req.method} ${req.url}`);
-//     next();
-// } );
-
+// define middleware that logs all incoming requests
 app.use(logger("dev"));
-app.use(express.static(path.join(__dirname, 'public') ));
+
+// define middleware that serves static resources in the public directory
+app.use(express.static(__dirname + '/public'));
+
+//Configure auth 
+// const config = {
+//     authRequired: false,
+//     auth0Logout: true,
+//     secret: process.env.AUTH0_SECRET,
+//     baseURL: process.env.AUTH0_BASE_URL,
+//     clientID: process.env.AUTH0_CLIENT_ID,
+//     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+// };
+// // auth router attaches /login, /logout, and /callback routes to the baseURL
+// app.use(auth(config));
+
+// // define middleware that appends useful auth-related information to the res object
+// // so EJS can easily access it
+// app.use((req, res, next) => {
+//     res.locals.isLoggedIn = req.oidc.isAuthenticated();
+//     res.locals.user = req.oidc.user;
+//     next();
+// })
+
+// // req.isAuthenticated is provided from the auth router
+// app.get('/authtest', (req, res) => {
+//     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
 
 // define a route for the default home page
 // Root Route
